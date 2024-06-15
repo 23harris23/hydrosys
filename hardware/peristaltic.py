216f48dc75d18cdedb1eecb34dc508ad1a0faa16
23harris23 <99044593+23harris23@users.noreply.github.com>
2024-06-15 16:56:51 -0700

    Update peristaltic.py
    added pump index and ability to name pumps

--- hardware/peristaltic.py
+++ hardware/peristaltic.py
@@ -1,11 +1,25 @@
 from time import sleep_ms
 
 
-class Pump:
-    def __init__(self, on_callback, off_callback, corrective_factor = 800):
+class pump_index:
+    master_dict = {}
+    def activate_pump(target_pump):
+        pump_index.master_dict[target_pump].on_callback()
+    def deactivate_pump(target_pump):
+        pump_index.master_dict[target_pump].off_callback()
+    def rename_pump(target_pump, name):
+        pump_index.master_dict[target_pump].name = name
+        pump_index.master_dict[name] = pump_index.master_dict.pop(target_pump)
+    
+class pump(pump_index):
+    def __init__(self, on_callback, off_callback, name = None, corrective_factor = 800):
         self.CF = corrective_factor
         self.on_callback = on_callback
         self.off_callback = off_callback
+        self.name = name
+        if self.name is None:
+            self.name = len(self.master_dict)
+        self.master_dict[self.name] = self
     def calibrate(self, target_mL, actual_mL):
         multiplier = target_mL / actual_mL
         self.CF = self.CF * multiplier
