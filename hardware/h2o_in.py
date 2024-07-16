ed8bcf8c8958f506dda6294821dc3f34909a0f24
23harris23 <99044593+23harris23@users.noreply.github.com>
2024-07-15 21:39:03 -0700

    Update h2o_in.py
    fixed bug in fill_quantity() added features

--- hardware/h2o_in.py
+++ hardware/h2o_in.py
@@ -6,7 +6,7 @@ class water_inlet_valve:
                  valve_pin,
                  float_sensor_callback = None,
                  tank_capacity = 30,
-                 ms_per_unit = 300):
+                 ms_per_unit = 14490):
         if float_sensor_callback is None:
             self.float_sensor_callback = self.default_float_sensor_callback
         else:
@@ -30,37 +30,42 @@ class water_inlet_valve:
         self.default_float_sensor_callback()
     def timer_wrapper(self, timer):
         self.off()
+        print('done')
     def default_float_sensor_callback(self):
         #float sensor gets false positives from software shutoffs find method to correct error
         print('Float sensor triggered')
     def get_fill_time(self):
         start_time = ticks_ms()
+        time_ms = 0
         self.on()
         while self.float_sensor_state == True:
             pass
         total_time = ticks_diff(ticks_ms(), start_time)
         return total_time
-    def get_fill_rate(self):
+    def calibrate_fill_rate(self):
         fill_time = self.get_fill_time()
         self.ms_per_unit = round(fill_time / self.tank_capacity)
         return self.ms_per_unit
     def fill_quantity(self, ammount):
-        if ammount < self.tank_capacity:
+        if ammount > self.tank_capacity:
             ammount = self.tank_capacity
             #Add error functionality
         fill_time = round(ammount * self.ms_per_unit)
         fill_timer = Timer(0)
         self.on()
         fill_timer.init(period = fill_time, mode = Timer.ONE_SHOT, callback = self.timer_wrapper)
-        print('done')
+    def get_fill_rate(self):
+        return self.ms_per_unit
+    def set_fill_rate(self, fill_rate):
+        self.ms_per_unit = fill_rate
 
 if __name__ == '__main__':
     valve1 = water_inlet_valve(23)
     #valve1.on()
     #fill_time = valve1.get_fill_time()
     #print(fill_time)
-    fill_rate = valve1.get_fill_rate()
+    fill_rate = valve1.calibrate_fill_rate()
     print(fill_rate)
-    sleep_ms(1000)
-    valve1.fill_quantity(10)
+    #sleep_ms(1000)
+    #valve1.fill_quantity(1)
 
