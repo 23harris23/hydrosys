73d82731f2f56626774c4656d8ebfdaf9a09e85c
23harris23 <99044593+23harris23@users.noreply.github.com>
2024-07-17 18:31:26 -0700

    Update peristaltic.py
    Removed pump index

--- hardware/peristaltic.py
+++ hardware/peristaltic.py
@@ -1,25 +1,12 @@
 from time import sleep_ms
 
 
-class pump_index:
-    master_dict = {}
-    def activate_pump(target_pump):
-        pump_index.master_dict[target_pump].on_callback()
-    def deactivate_pump(target_pump):
-        pump_index.master_dict[target_pump].off_callback()
-    def rename_pump(target_pump, name):
-        pump_index.master_dict[target_pump].name = name
-        pump_index.master_dict[name] = pump_index.master_dict.pop(target_pump)
-    
-class pump(pump_index):
-    def __init__(self, on_callback, off_callback, name = None, corrective_factor = 800):
+class pump():
+    def __init__(self, on_callback, off_callback, name, corrective_factor = 800):
         self.CF = corrective_factor
         self.on_callback = on_callback
         self.off_callback = off_callback
         self.name = name
-        if self.name is None:
-            self.name = len(self.master_dict)
-        self.master_dict[self.name] = self
     def calibrate(self, target_mL, actual_mL):
         multiplier = target_mL / actual_mL
         self.CF = self.CF * multiplier
@@ -33,3 +20,19 @@ class pump(pump_index):
         self.on_callback()
         sleep_ms(duration)
         self.off_callback()
+    def rename(self, name):
+        self.name = name
+    def get_corrective_factor(self):
+        return self.CF
+    def get_name(self):
+        return self.name
+    def get_config(self):
+        return (self.name, self.CF)
+    def set_config(self, config):
+        self.name = config[0]
+        self.CF = config[1]
+
+def instalize_595_pump(ID, shift_register, name):
+    on_callback = lambda: shift_register.activate_pin(ID)
+    off_callback = shift_register.clear_all
+    return pump(on_callback, off_callback, name)
