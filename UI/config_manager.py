47808779d56c7ef8d94b08678fd0a014733eadf3
23harris23 <99044593+23harris23@users.noreply.github.com>
2024-08-01 19:51:06 -0700

    Update config_manager.py
    added functionality to support implimentation of watering presets

--- UI/config_manager.py
+++ UI/config_manager.py
@@ -1,40 +1,58 @@
 import json
+from collections import OrderedDict
 
 
 class config_file:
-    PATH = 'conf.json'
-    DEFAULT_CONF = '{"test": 0}'
-    def get_config_data():
+    DEFAULT_PATH = 'conf.json'
+    DEFAULT_CONF = '{}'
+    def __init__(self, path):
+        self.path = path
+    def get_config_data(self):
         try:
-            f = open(config_file.PATH, 'r')
+            f = open(self.path, 'r')
             data = f.read()
             f.close()
         except OSError:
-            f = open(config_file.PATH, 'w')
+            f = open(self.path, 'w')
             f.write(config_file.DEFAULT_CONF)
             f.close()
             data = config_file.DEFAULT_CONF
         return data
-    def parse_config():
-        raw_conf_data = config_file.get_config_data()
+    def parse_config(self):
+        raw_conf_data = self.get_config_data()
         json_data = json.loads(raw_conf_data)
         return json_data
-    def get_config_entry(config_key):
-        conf_json = config_file.parse_config()
+    def get_config_entry(self, config_key):
+        conf_json = self.parse_config()
         if config_key in conf_json.keys():
             return conf_json[config_key]
         return None
-    def set_config_entry(name, config_value):
-        json_data = config_file.parse_config()
+    def set_config_entry(self, name, config_value):
+        json_data = self.parse_config()
         json_data[name] = config_value
         raw_data = json.dumps(json_data)
-        f = open(config_file.PATH, 'w')
+        f = open(self.path, 'w')
         f.write(raw_data)
         f.close()
-        
+
+class watering_profile_config(config_file):
+    def __init__(self, path):
+        super().__init__(path)
+    def get_watering_profiles(self):
+        config_data = self.parse_config()
+        watering_profile_names = list(config_data.keys())
+        return watering_profile_names
+    def get_watering_data(self, watering_profile_name):
+        watering_profile_data = self.get_config_entry(watering_profile_name)
+        profile_data = OrderedDict(reversed(list(watering_profile_data.items())))
+        return profile_data
+    def update_watering_profile(self, profile_name, profile_data):
+        print(profile_data)
+        self.set_config_entry(profile_name, OrderedDict(profile_data))
 
 class config_item(config_file):
-    def __init__(self, get_item_callback, set_item_callback, name):
+    def __init__(self, path, name, get_item_callback, set_item_callback):
+        super().__init__(path)
         self.get_item_callback = get_item_callback
         self.set_item_callback = set_item_callback
         self.name = name
@@ -44,14 +62,14 @@ class config_item(config_file):
         return value
     def set_item(self, value):
         self.set_item_callback(value)
-        config_file.set_config_entry(self.name, value)
+        self.set_config_entry(self.name, value)
     def check_config_file(self):
-        entry = config_file.get_config_entry(self.name)
+        entry = self.get_config_entry(self.name)
         if entry is not None:
             self.set_item(entry)
         else:
             entry = self.get_item_callback()
-            config_file.set_config_entry(self.name, self.get_item_callback())
+            self.set_config_entry(self.name, self.get_item_callback())
     def update_config_value(self):
         self.set_item(self.get_value())
-
+        
